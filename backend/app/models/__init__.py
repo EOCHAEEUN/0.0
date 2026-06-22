d43e3f9c42fbf1241badefedbe6e7fd1330fe9f9
kanswoo <ko4861q@gmail.com>
2026-06-22 12:50:24 +0900

    feat:models/ 안에 safety~~파일 생성 .common은 literal임포트 파일 safety는 현재 백엔드쪽 사용중이라 삭제하면안돼서 남겨놨어요

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,13 @@
+from app.models.safety_check_status import (
+    SafetyCheckStatus,
+    SafetyCheckStatusSaveRequest,
+)
+from app.models.safety_rule_legal import SafetyRuleLegal
+from app.models.safety_rule_voluntary import SafetyRuleVoluntary
+
+__all__ = [
+    "SafetyCheckStatus",
+    "SafetyCheckStatusSaveRequest",
+    "SafetyRuleLegal",
+    "SafetyRuleVoluntary",
+]
